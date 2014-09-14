445b861bfb4cf7d9ed343c92574afe90d53c2bf6
bdusell <bdusell@gmail.com>
2014-09-14 16:44:18 -0700

    Revise array and func utils.

--- src/lib/ArrayUtil.php
+++ src/lib/ArrayUtil.php
@@ -8,13 +8,13 @@ class ArrayUtil {
 	 *
 	 * The signature of the callback is expected to be:
 	 * `function(&$value, $key [, $&userdata])` */
-	public static function dfs(&$array, $callback, $userdata = null) {
+	public static function apply_depth_first(&$array, $callback, $userdata = null) {
 		array_walk_recursive($array, $callback, $userdata);
 	}
 
 	/* Return the number of leaf elements in a recursive array
 	 * structure. */
-	public static function num_leaves($array) {
+	public static function count_leaves($array) {
 		return count($array, COUNT_RECURSIVE);
 	}
 }
--- src/lib/FuncUtil.php
+++ src/lib/FuncUtil.php
@@ -11,30 +11,6 @@ class FuncUtil {
 	public static function string_cmp($a, $b) {
 		return strcmp($a, $b);
 	}
-
-	private $_int_number = 0;
-	private $_string_number = 1;
-
-	private static function _key_type_number($v) {
-		return is_int($v) ? self::$_int_number : self::$_string_number;
-	}
-
-	/* A comparison function defining a total ordering for integers and
-	 * strings (valid PHP array keys), using strict comparison. */
-	public static function key_cmp($a, $b) {
-		$value = self::int_cmp(
-			$type = self::_key_type_number($a),
-			self::_key_type_number($b)
-		);
-		if($value == 0) {
-			if($type == self::$_int_number) {
-				return self::int_cmp($a, $b);
-			} elseif($type == self::$_string_number) {
-				return self::string_cmp($a, $b);
-			}
-		}
-		return $value;
-	}
 }
 
 ?>

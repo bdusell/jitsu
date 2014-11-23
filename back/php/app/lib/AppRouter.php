14c13c9e83f95132b9bd444dfd8ba6ca69e662ee
bdusell <bdusell@gmail.com>
2014-11-22 16:07:02 -0800

    Update example router.

--- back/php/app/lib/AppRouter.php
+++ back/php/app/lib/AppRouter.php
@@ -3,18 +3,17 @@
 class AppRouter extends Router {
 
 	public function routes() {
-		$this->map('GET', '', array('StaticPagesController', 'home'));
-		$this->map('GET', 'home', array('StaticPagesController', 'home'));
-		$this->map('GET', 'lists', array('ListController', 'index'));
-		$this->map('POST', 'lists', array('ListController', 'create'));
-		$this->map('GET', 'lists/:id', array('ListController', 'show'));
-		$this->map('GET', 'cards', array('CardController', 'index'));
-		$this->map('POST', 'cards', array('CardController', 'create'));
-		$this->map('PUT', 'cards', array('CardController', 'edit'));
-		$this->map('GET', 'cards/:id', array('CardController', 'show'));
-		$this->map('GET', 'lists/:id/cards', array('ListCardController', 'index'));
-		$this->map('POST', 'lists/:id/cards', array('ListCardController', 'create'));
-		$this->map('GET', 'lists/:id/cards/:id', array('ListCardController', 'show'));
+		$this->map('GET',    '(home)', array('StaticPagesController', 'home'));
+		$this->map('GET',    'videos/', array('VideosController', 'index'));
+		$this->map('GET',    'videos/new', array('VideosController', 'new'));
+		$this->map('POST',   'videos/', array('VideosController', 'create'));
+		$this->map('GET',    'videos/:id', array('VideosController', 'show'));
+		$this->map('GET',    'videos/:id/edit', array('VideosController', 'edit'));
+		$this->map('PUT',    'videos/:id', array('VideosController', 'update'));
+		$this->map('PATCH',  'videos/:id', array('VideosController', 'patch'));
+		$this->map('DELETE', 'videos/:id', array('VideosController', 'delete'));
+		$this->map('POST',   'videos/:id/tags/', array('TagsController', 'create'));
+		$this->map('DELETE', 'videos/:id/tags/:value', array('TagsController', 'delete'));
 	}
 }
 

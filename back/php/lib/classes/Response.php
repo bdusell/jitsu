549c6a3b382532eba7a82fbbc23feaace4784611
bdusell <bdusell@gmail.com>
2014-11-25 23:24:46 -0800

    Enhance Response class.

--- back/php/lib/classes/Response.php
+++ back/php/lib/classes/Response.php
@@ -1,43 +1,67 @@
 <?php
 
+/* Utilities for building the response sent back to the client. */
 class Response {
 
+	/* Set the HTTP response code of the response. If `$code` is null,
+	 * return the currently set response code. */
 	public static function code($code = null) {
-		if(is_null($code)) return http_response_code();
+		if($code === null) return http_response_code();
 		else return http_response_code($code);
 	}
 
+	/* Set a header in the response. Overwrites any previous header with
+	 * the same name. Must be called before output is written, just like
+	 * PHP `header`. */
 	public static function header($name, $value) {
 		header("$name: $value");
 	}
 
-	public static function type($type) {
+	/* Set the content type of the response using the `Content-Type`
+	 * header. */
+	public static function content_type($type) {
 		self::header('Content-Type', $type);
 	}
 
-	public static function cookie($name, $value, $lifespan = null, $path = null) {
+	/* Send a cookie back to the client. Provide a lifespan in seconds from
+	 * the current time; if this is null, the cookie has an unlimited
+	 * lifespan. Optionally provide a domain and path for the cookie. */
+	public static function cookie($name, $value, $lifespan = null, $domain = null, $path = null) {
 		setcookie(
 			$name,
 			$value,
 			$lifespan === null ? 0 : time() + $lifespan,
-			$path === null ? '' : $path
+			$path,
+			$domain
 		);
 	}
 
-	public static function delete_cookie($name, $path = null) {
-		self::cookie($name, '', -3600, $path);
+	/* Request that a cookie be deleted from the client. */
+	public static function delete_cookie($name, $domain = null, $path = null) {
+		self::cookie($name, '', -3600, $domain, $path);
 	}
 
+	/* Shorthand for sending a PHP array as a JSON object in the
+	 * response. */
 	public static function json($obj, $pretty = true) {
-		self::type('application/json');
+		self::content_type('application/json');
 		echo Serialize::json($obj, $pretty);
 	}
 
+	/* Shorthand for sending a file with a given content type in the
+	 * response. */
 	public static function file($path, $content_type) {
-		self::type($content_type);
+		self::content_type($content_type);
 		readfile($path);
 	}
 
+	/* Issue a redirect to another URL. Note that this does NOT exit the
+	 * current process. Keep in mind that relying on the client to respect
+	 * this header and close the connection for you is a huge security
+	 * hole.
+	 *
+	 * The response code should be a 3XX code.
+	 */
 	public static function redirect($url, $code) {
 		header('Location: ' . $url, true, $code);
 	}

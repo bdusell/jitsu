00a13fe5c211f1fcab73b1f978843b0b6a49833d
bdusell <bdusell@gmail.com>
2014-12-31 15:51:13 -0800

    Update MetaUtil.

--- src/php/lib/classes/MetaUtil.php
+++ src/php/lib/classes/MetaUtil.php
@@ -9,6 +9,28 @@ class MetaUtil {
 	public static function function_exists($name) {
 		return function_exists($name);
 	}
+
+	public static function call_function($name /* $arg1, ... */) {
+		$args = func_get_args();
+		array_shift($args);
+		return call_user_func_array($name, $args);
+	}
+
+	public static function apply_function($name, $args) {
+		return call_user_func_array($name, $args);
+	}
+
+	public static function apply_method($obj, $name, $args) {
+		return call_user_func_array(array($obj, $name), $args);
+	}
+
+	public static function apply_static_method($class_name, $name, $args) {
+		return call_user_func_array(array($class_name, $name), $args);
+	}
+
+	public static function apply_constructor($class_name, $args) {
+		(new ReflectionClass($class_name))->newInstanceArgs($args);
+	}
 }
 
 ?>

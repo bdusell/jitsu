5f3ebdf4838b8a4656a1afabee37e1a88282d38e
bdusell <bdusell@gmail.com>
2014-12-31 16:31:45 -0800

    Correctly use Bootstrap thumbnail class on video index page.

--- src/php/app/views/videos/index.html.php
+++ src/php/app/views/videos/index.html.php
@@ -4,25 +4,21 @@
   foreach($videos as $video):
 ?>
   <div class="col-xs-6 col-md-2">
-    <div class="video-cell-container">
-      <div class="video-cell">
-        <a href="videos/<?= html($video->id) ?>">
-          <h2><?= html($video->name) ?></h2>
-          <p><?= html($video->id) ?></p>
-        </a>
+    <a class="thumbnail" href="videos/<?= html($video->id) ?>">
+      <div class="caption">
+        <h2><?= html($video->name) ?></h2>
+        <p><?= html($video->id) ?></p>
       </div>
-    </div>
+    </a>
   </div>
 <?php
   endforeach;
 ?>
   <div class="col-xs-6 col-md-2">
-    <div class="video-cell-container">
-      <div class="video-cell">
-        <a href="videos/new">
-          <h2>Add New Video</h2>
-        </a>
+    <a class="thumbnail" href="videos/new">
+      <div class="caption">
+        <h2>Add New Video</h2>
       </div>
-    </div>
+    </a>
   </div>
 </div>

0dc42426fbbf60a68258799a48a046d61bb75714
bdusell <bdusell@gmail.com>
2015-01-18 17:39:16 -0600

    Update Database helper.

--- src/app/helpers/Database.php
+++ src/app/helpers/Database.php
@@ -2,21 +2,26 @@
 
 class Database {
 
-	private static $instance = null;
+	private static $_instance = null;
+	private static $visitor;
 
 	private static function create_instance() {
 		switch(config::sql_driver()) {
 		case 'mysql':
-			return new \phrame\sql\MysqlDatabase(
+			$db = new \phrame\sql\MysqlDatabase(
 				config::db_host(),
 				config::db_name(),
 				config::db_user(),
 				config::db_password()
 			);
+			self::$visitor = new \phrame\sql\ast\MysqlVisitor($db);
+			return $db;
 		case 'sqlite':
-			return new \phrame\sql\SqliteDatabase(
+			$db = new \phrame\sql\SqliteDatabase(
 				config::sqlite_file()
 			);
+			self::$visitor = new \phrame\sql\ast\SqliteVisitor($db);
+			return $db;
 		default:
 			throw new \phrame\ConfigurationError(
 				'unsupported SQL driver ' . Util::repr(config::sql_driver())
@@ -24,16 +29,21 @@ class Database {
 		}
 	}
 
-	private static function get_instance() {
-		if(self::$instance === null) {
-			self::$instance = self::create_instance();
+	public static function instance() {
+		if(self::$_instance === null) {
+			self::$_instance = self::create_instance();
 		}
-		return self::$instance;
+		return self::$_instance;
+	}
+
+	public static function interpret($n) {
+		self::instance();
+		return $n->accept(self::$visitor);
 	}
 
 	public static function __callStatic($name, $args) {
 		return call_user_func_array(
-			array(self::get_instance(), $name),
+			array(self::instance(), $name),
 			$args
 		);
 	}

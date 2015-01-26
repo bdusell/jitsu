5d2b4c3dd0ba7616ad54aee8f7147d46d4f14e5f
bdusell <bdusell@gmail.com>
2015-01-26 03:33:54 -0600

    Save work on precedence visitors.

--- lib/php/classes/phrame/sql/visitors/MysqlVisitor.php
+++ lib/php/classes/phrame/sql/visitors/MysqlVisitor.php
@@ -4,8 +4,19 @@ namespace phrame\sql\visitors;
 
 class MysqlVisitor extends CodeGenerationVisitor {
 
+	public function __construct($database) {
+		parent::__construct($database, new MysqlPrecedenceVisitor());
+	}
+
 	public function visitIdentifier($n) {
 		return '`' . str_replace('`', '``', $n->value) . '`';
+	}
+
+	public function visitConcatenationExpression($n) {
+		return (
+			'CONCAT(' . $n->left->accept($this) . ', ' .
+			$n->right->accept($this) . ')'
+		);
 	}
 }
 
--- lib/php/classes/phrame/sql/visitors/SqliteVisitor.php
+++ lib/php/classes/phrame/sql/visitors/SqliteVisitor.php
@@ -4,6 +4,9 @@ namespace phrame\sql\visitors;
 
 class SqliteVisitor extends CodeGenerationVisitor {
 
+	public function __construct($database) {
+		parent::__construct($database, new SqlitePrecedenceVisitor());
+	}
 }
 
 ?>

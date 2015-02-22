9f9755a9e707d949045337f4efb210f109319154
bdusell <bdusell@gmail.com>
2015-02-22 01:19:11 -0600

    Work on tag styling in demo.

--- demo/src/js/views/tags/show.js
+++ demo/src/js/views/tags/show.js
@@ -4,9 +4,8 @@ var Backbone = require('../../shim/backbone');
 module.exports = Backbone.View.extend({
   tagName: 'li',
   render: function() {
-    this.$el.text(this.model.get('value')).append(
-      $('<span>').addClass('x-button')
-    );
+    this.$el.text(this.model.get('value'))
+      .append($('<span>').addClass('x-button'));
     return this;
   },
   events: {
